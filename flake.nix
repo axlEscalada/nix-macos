33bbd6114897890e6663bcbf0ada7465b298f2c5
axlEscalada <axescalada@gmail.com>
2024-05-24 12:53:08 -0300

    add flakes for macos that includes a custom nixvim

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Axel's dotfiles";
+
+  # inputs are other flakes you use within your own flake, dependencies
+  # if you will
+  inputs = {
+    # unstable has the 'freshest' packages you will find, even the AUR
+    # doesn't do as good as this, and it's all precompiled.
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixvim.url = "github:axlEscalada/nixvim";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  # In this context, outputs are mostly about getting home-manager what it
+  # needs since it will be the one using the flake
+  outputs = { nixpkgs, home-manager, nixvim, ... }: {
+    homeConfigurations = {
+      "axel.escalada" = home-manager.lib.homeManagerConfiguration {
+        # darwin is the macOS kernel and aarch64 means ARM, i.e. apple silicon
+        pkgs = nixpkgs.legacyPackages.aarch64-darwin;
+        modules = [ ./home.nix ];
+        extraSpecialArgs = {
+          inherit nixvim;
+        };
+      };
+    };
+  };
+}
---
+++ home.nix
@@ -0,0 +1,14 @@
+{ pkgs, nixvim, ... }: {
+  # This is required information for home-manager to do its job
+  home = {
+    stateVersion = "23.11";
+    username = "axel.escalada";
+    homeDirectory = "/Users/axel.escalada";
+    packages = [
+      nixvim.packages.${pkgs.system}.default
+    ];
+  };
+  xdg.enable = true;
+  programs.home-manager.enable = true;
+  programs.fish.enable = true;
+}
